d533e593706e09e365e1b5f595158f7fb799d1c3
Chris Schilling <schillingc007@gmail.com>
2023-04-07 23:47:08 -0400

    Game Engine Update
    Added in Pause panel, start of enemy spawn.

--- LameJam/Assets/Scripts/GameEventHandler.cs
+++ LameJam/Assets/Scripts/GameEventHandler.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
+using System;
+using JetBrains.Annotations;
 
 public class GameEventHandler : MonoBehaviour
 {
@@ -11,6 +13,10 @@ public class GameEventHandler : MonoBehaviour
     [SerializeField] private GameObject PausePanel;
     [SerializeField] private GameObject LeftHand;
     [SerializeField] private GameObject RightHand;
+    [SerializeField] private CreateNewEnemy[] enemyDataList;
+    [SerializeField] private float minSpawnDelay = 2.0f;
+    [SerializeField] private float maxSpawnDelay = 5.0f;
+    [SerializeField] private float spawnRateIncreasePerSecond = 0.1f;
 
     //Button Event Manager
     [SerializeField] public Button mainMenuBtn;
@@ -22,6 +28,12 @@ public class GameEventHandler : MonoBehaviour
     [SerializeField] private TextMeshProUGUI timerText;
 
     private float timeRemaining;
+
+    // Enemy spawn fields
+    private float spawnDelay = 2.0f;
+    private float timeLeft = 0.0f;
+
+    // Game state
     private bool gameStarted = false;
 
 
@@ -48,6 +60,15 @@ public class GameEventHandler : MonoBehaviour
             }
 
             UpdateTimerText();
+
+            if (timeLeft < timeRemaining)
+            {
+                spawnDelay = Mathf.Clamp(spawnDelay - spawnRateIncreasePerSecond * Time.deltaTime, minSpawnDelay, maxSpawnDelay);
+                if (UnityEngine.Random.value < Time.deltaTime / spawnDelay)
+                {
+                    SpawnEnemy();
+                }
+            }
         }
         else { Time.timeScale = 0f; }
 
@@ -57,6 +78,7 @@ public class GameEventHandler : MonoBehaviour
         }
     }
 
+    #region PauseMenu
     private void PauseGame()
     {
         Debug.Log("Game Paused");
@@ -75,6 +97,8 @@ public class GameEventHandler : MonoBehaviour
         LeftHand.GetComponent<RotateClockHands>().Pause();
         RightHand.GetComponent<RotateClockHands>().Pause();
     }
+    #endregion
+
 
     #region ButtonClicks
     private void OnMainMenuButtonClicked()
@@ -94,6 +118,7 @@ public class GameEventHandler : MonoBehaviour
     }
     #endregion
 
+    
     private void StartGame()
     {
         Debug.Log("Game Started");
@@ -108,4 +133,25 @@ public class GameEventHandler : MonoBehaviour
         Debug.Log("Game Ended");
         gameStarted = false;
     }
+
+
+    private void SpawnEnemy()
+    {
+        if (gameStarted)
+        {
+            // Select a random enemy prefab from the enemyPrefabs array
+           CreateNewEnemy enemyData = enemyDataList[UnityEngine.Random.Range(0, enemyDataList.Length)];
+
+            // Spawn the enemy at a random position within the spawnZone
+            float minX = -5f; // Minimum value for x
+            float maxX = 5f;  // Maximum value for x
+            float y = 5f;     // Static value for y
+            float z = 0f;     // Static value for z
+
+            Vector3 randomPosition = new Vector3(UnityEngine.Random.Range(minX, maxX), y, z);
+            enemyData.SpawnEnemy(randomPosition);
+        }
+    }
+
+
 }
